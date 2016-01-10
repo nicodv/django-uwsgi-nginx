8706ea869f25792ec22339a66ce453573a9167e2
nico <njdevos@gmail.com>
2016-01-09 22:54:26 -0800

    set STATIC_ROOT

--- djangosite/djangosite/settings/base.py
+++ djangosite/djangosite/settings/base.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
