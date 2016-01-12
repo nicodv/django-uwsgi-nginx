ada345d35568887a5b0f35262405664126674a1c
Nico de Vos <njdevos@gmail.com>
2016-01-11 22:58:33 -0800

    Update urls.py
    cleanup

--- djangosite/djangosite/urls.py
+++ djangosite/djangosite/urls.py
@@ -17,11 +17,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from djangosite import views
 import helloworldapp
 
 urlpatterns = [
-    url(r'^$', views.homepage),
     url(r'^admin/', admin.site.urls),
     url(r'^helloworld/', include('helloworldapp.urls')),
 ]
